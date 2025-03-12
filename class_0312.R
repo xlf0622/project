262045dc111400f09daff3cbe5f0bb3ab49431a0
xlf0622 <1552454077@qq.com>
2025-03-12 11:37:56 +0800

    class_0312

---
+++ class_0312.R
@@ -0,0 +1,16 @@
+# HEADER --------------------------------------------
+#
+# Author:     xlf
+# Copyright     Copyright 2025 - Name Surname
+# Email:      yourmail@example.com
+#
+# Date:     2025-03-12
+#
+# Script Name: class_0312   
+#
+# Script Description:
+#
+#
+# SETUP ------------------------------------
+cat("\014")                 # Clears the console
+rm(list = ls())             # Remove all variables of the work space
